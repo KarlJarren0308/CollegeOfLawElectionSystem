400c40d3853b60aabbcd735699e754ef55056015
Karl Macadangdang <karljarren0308@yahoo.com>
2015-06-28 11:24:52 +0800

    Applied Changes

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,3 +1,5 @@
+var info = [];
+
 function loadMessages() {
     $.ajax({
         url: 'requests/logs.php',
@@ -13,7 +15,18 @@ function loadMessages() {
     return false;
 }
 
-function revealResults() {
+function revealResults(pos) {
+    for(var i = 0; i < info.length; i++) {
+        if(pos == info[i]['position']) {
+            var arr = $('#count-' + info[i]['id']).text().split('/');
+            var newWidth = (arr[0] / arr[1]) * 100;
+
+            $('#count-' + info[i]['id']).fadeIn(500);
+            $('#bar-' + info[i]['id']).animate({
+                'width': newWidth + '%'
+            }, 1500);
+        }
+    }
 }
 
 $(document).ready(function() {
@@ -64,11 +77,60 @@ $(document).ready(function() {
                 $('.candidates').css({
                     'margin-top': -($('.candidates').height() / 2) + 'px'
                 });
+
+                $.ajax({
+                    url: 'requests/get_candidate_id.php',
+                    method: 'GET',
+                    dataType: 'json',
+                    success: function(response) {
+                        info = response;
+                    }
+                });
+                
+                return false;
             }
         });
     });
 
     $('#reveal-button').click(function() {
-        revealResults('.bar');
+        var pos = '';
+
+        switch($('#position').val()) {
+            case '0':
+                pos = 'President';
+                break;
+            case '1':
+                pos = 'Vice President';
+                break;
+            case '2':
+                pos = 'Secretary';
+                break;
+            case '3':
+                pos = 'Treasurer';
+                break;
+            case '4':
+                pos = 'Auditor';
+                break;
+            case '5':
+                pos = 'Business Manager';
+                break;
+            case '6':
+                pos = '4th Year Representative';
+                break;
+            case '7':
+                pos = '3rd Year Representative';
+                break;
+            case '8':
+                pos = '2nd Year Representative';
+                break;
+            case '9':
+                pos = '1st Year Representative';
+                break;
+            default:
+                pos = '';
+                break;
+        }
+
+        revealResults(pos);
     });
 });
